ebc160d598b3882682a1753cadaff332aab3623b
Emmanuel Ramirez <emmanuelraob123@gmail.com>
2023-12-31 00:15:22 -0600

    setting up de compile c in cython

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
-# setup.py
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
+# Define una extensión con opciones de compilación adicionales para ball.pyx
+extensions = [
+    Extension(
+        "ball",  # Nombre del módulo
+        ["ball.pyx"],
+        extra_compile_args=["-O3"],  # Opciones de compilación adicionales
+        extra_link_args=["-O3"]
+    )
+]
+
 setup(
-    ext_modules = cythonize("ball.pyx")
+    name="Nombre",
+    ext_modules=cythonize(extensions)
 )
--- using_cython.py
+++ using_cython.py
@@ -1,15 +1,37 @@
+import os
+import sys
+import subprocess
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import time
+
+def build_cython_modules():
+    # Comprueba si los archivos .pyx han sido modificados más recientemente que sus .c correspondientes
+    need_rebuild = False
+    cython_files = ["ball.pyx"]
+    for file in cython_files:
+        c_file = file.replace('.pyx', '.c')
+        if (not os.path.exists(c_file) or
+            os.path.getmtime(file) > os.path.getmtime(c_file)):
+            need_rebuild = True
+            break
+
+    # Si es necesario, recompila los módulos Cython
+    if need_rebuild:
+        print("Recompilando módulos Cython...")
+        subprocess.run([sys.executable, "setup.py", "build_ext", "--inplace"])
+
+build_cython_modules()
+
 import ball  # Asegúrate de que este módulo esté compilado y disponible
 
 # Configuraciones iniciales
 width, height = 800, 600
 max_balls = 1000
-ball_radius = 0.002
-speed = 0.01
+ball_radius = 1/width * 2
+speed = 1/width * 3
 
 # Inicialización de Pygame y OpenGL
 pygame.init()
